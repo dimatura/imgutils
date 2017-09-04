bec87b712d23344eaa35d8e3cbdc48e6fe8b4669
Daniel Maturana <dimatura@cmu.edu>
2017-09-04 18:22:47 -0400

    version bump

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='imgutils',
-    version='0.1.1',
+    version='0.1.2',
     description="Miscellaneous image utility functions.",
     long_description=readme,
     author="Daniel Maturana",
